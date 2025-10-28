c47d16b400e05fecf16d4697da75625d6dcab058
Magdalena Szymura <mag.szymura@gmail.com>
2025-10-28 20:09:19 +0100

    SAMOUK-2 feat: add input and login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { PATHS } from '../constants/paths';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Login,
+    pathMatch: 'full',
+  },
+];
---
+++ src/app/login/login.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Input } from '../form/input/input';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, Input],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+  onSubmit() {
+    if (this.form.valid) {
+      const email = this.form.value.email;
+      const password = this.form.value.password;
+      console.log('Email:', email);
+      console.log('Password:', password);
+    }
+  }
+}
