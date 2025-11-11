b1a9fe12d9a909d20d212516d1ee8d33679f96f3
Magdalena Szymura <mag.szymura@gmail.com>
2025-11-11 11:36:38 +0100

    SAMOUK-3 feat: add navbar and dashboard components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './login/login';
 import { PATHS } from '../constants/paths';
 import { PageNotFoundComponent } from './page-not-found-component/page-not-found-component';
+import { Dashboard } from './dashboard/dashboard';
 
 export const routes: Routes = [
   { path: PATHS.LOGIN, component: LoginPage },
+  { path: PATHS.DASHBOARD, component: Dashboard },
   { path: '**', component: PageNotFoundComponent },
 ];
